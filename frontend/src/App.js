8425f43853f03345457e32881bc221b529377c60
Angel <angel.villalobos2596@gmail.com>
2022-06-03 23:50:31 -0600

    Bootstrap and sass added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Test from './components/Test';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Test/>
     </div>
   );
 }
---
+++ frontend/src/components/Test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Test.scss";
+
+const Test = () => {
+  return (
+    <div className="test">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-6">
+            <p>Test</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Test;
