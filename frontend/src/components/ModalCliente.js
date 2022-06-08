aea311cdaa1e05d5fefacf20401687120d631c9c
Angel <angel.villalobos2596@gmail.com>
2022-06-07 20:43:52 -0600

    update

--- frontend/src/components/ModalCliente.js
+++ frontend/src/components/ModalCliente.js
@@ -1,29 +1,28 @@
-import React, {useState} from 'react'
-import Modal from 'react-bootstrap/Modal'
-import Button from 'react-bootstrap/Button'
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
+const ModalCliente = ({ isShow, handleClose }) => {
 
-const ModalCliente = ({isshow, handleClose}) => {
-    //const [show, setShow] = useState(isshow);
+  //const [show, setShow] = useState(isshow);
 
-   
-    //const handleShow = () => setShow(true);
+  //const handleShow = () => setShow(true);
 
-    return (
-        <Modal show={isshow} onHide={handleClose}>
-            <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-            <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-                Close
-            </Button>
-            {/*<Button variant="primary" onClick={isNew ? () => handleCreate() : () => handleEdit()}>{isNew ? 'Crear' : 'Guardar Cambios'}</Button>*/}
-            <Button variant="primary" >save changes</Button>
-            </Modal.Footer>
-        </Modal>
-    )
-}
+  return (
+    <Modal show={isShow} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Modal heading</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+        {/*<Button variant="primary" onClick={isNew ? () => handleCreate() : () => handleEdit()}>{isNew ? 'Crear' : 'Guardar Cambios'}</Button>*/}
+        <Button variant="primary">save changes</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
 
-export default ModalCliente
+export default ModalCliente;
